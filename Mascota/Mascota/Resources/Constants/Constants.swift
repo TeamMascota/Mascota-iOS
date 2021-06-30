ca8a024b5d3b86a0faa5bc6ebcf40e793bf40855
Yoonseo Kim <72497599+ezidayzi@users.noreply.github.com>
2021-06-30 17:45:11 +0900

    [#4] Constants 정의

---
+++ Mascota/Mascota/Resources/Constants/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Mascota
+//
+//  Created by 김윤서 on 2021/06/30.
+//
+
+import UIKit
+
+public enum Constant {
+    
+    // MARK: - Border 두께 줄때 사용
+    public static let border1: CGFloat = 1.0
+    
+    // MARK: - Corner Radius 값 줄 때 사용
+    public static let round3: CGFloat = 3.0
+    public static let round5: CGFloat = 5.0
+}
