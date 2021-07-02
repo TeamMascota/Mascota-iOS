ca2fb4f31369f391de92ec903e74cff16f236e1b
Yoonseo Kim <yo26on@soongsil.ac.kr>
2021-07-02 13:21:03 +0900

    [#4] RainbowViewController 생성

---
+++ Mascota/Mascota/Sources/ViewControllers/RainbowViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RainbowViewController.swift
+//  Mascota
+//
+//  Created by 김윤서 on 2021/07/01.
+//
+
+import UIKit
+
+class RainbowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
