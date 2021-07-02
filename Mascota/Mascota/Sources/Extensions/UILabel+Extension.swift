6d4d7380e90e348de365f05f5faeb6975c725de2
Yoonseo Kim <yo26on@soongsil.ac.kr>
2021-07-02 13:19:10 +0900

    [#4] UILabel LineSpacing Extension 추가

---
+++ Mascota/Mascota/Sources/Extensions/UILabel+Extension.swift
@@ -0,0 +1,30 @@
+//
+//  UILabel+Extension.swift
+//  Mascota
+//
+//  Created by 김윤서 on 2021/07/01.
+//
+
+import UIKit
+
+extension UILabel {
+    func setLineSpacing(lineSpacing: CGFloat = 0.0, lineHeightMultiple: CGFloat = 0.0) {
+
+        guard let labelText = self.text else { return }
+
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = lineSpacing
+        paragraphStyle.lineHeightMultiple = lineHeightMultiple
+
+        let attributedString: NSMutableAttributedString
+        if let labelattributedText = self.attributedText {
+            attributedString = NSMutableAttributedString(attributedString: labelattributedText)
+        } else {
+            attributedString = NSMutableAttributedString(string: labelText)
+        }
+
+        attributedString.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: NSRange(location: 0, length: attributedString.length))
+
+        self.attributedText = attributedString
+    }
+}
