0464bc9a9d8ba318ce67a92c5f633a1831e23e99
Yoonseo Kim <yo26on@soongsil.ac.kr>
2021-07-02 13:18:45 +0900

    [#4] ViewController 초기상태로

--- Mascota/Mascota/Sources/ViewControllers/ViewController.swift
+++ Mascota/Mascota/Sources/ViewControllers/ViewController.swift
@@ -7,21 +7,11 @@
 
 import UIKit
 
-import SnapKit
-
 class ViewController: UIViewController {
-
-    let bookView = BookPageView(type: .rainbow)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(bookView)
-        bookView.snp.makeConstraints {
-            $0.center.equalToSuperview()
-            $0.width.equalTo(300)
-            $0.height.equalTo(250)
-        }
-        // Do any additional setup after loading the view.
+      
     }
 
 }
