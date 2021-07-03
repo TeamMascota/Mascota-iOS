bca43706bdee8bc27f20f3eb44ad2034cdc9aa52
YeonSeoDo <ehdustj1001@naver.com>
2021-07-04 02:57:06 +0900

    [#22] 회원가입뷰 스토리보드 작성 및 IBOutlet연결

---
+++ Mascota/Mascota/Sources/ViewControllers/SignUpViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SignUpViewController.swift
+//  Mascota
+//
+//  Created by DYS on 2021/07/04.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    // MARK: - Properties
+    @IBOutlet weak var idEmailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordCheckTextField: UITextField!
+    @IBOutlet var underlineView: [UIView]!
+    @IBOutlet weak var signUpButton: UIButton!
+
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
