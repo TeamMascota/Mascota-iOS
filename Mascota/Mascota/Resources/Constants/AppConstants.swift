10b4b2b1cd448e8536b66b127e7896849881c8b5
jeongyeob97 <leejeongyeob97@gmail.com>
2021-07-04 20:00:31 +0900

    [#31] AppConstant 추가

--- Mascota/Mascota/Resources/Constants/AppConstants.swift
+++ Mascota/Mascota/Resources/Constants/AppConstants.swift
@@ -5,4 +5,24 @@
 //  Created by apple on 2021/06/29.
 //
 
-import Foundation
+import UIKit
+
+struct AppConstants {
+    struct Storyboard {
+        static let main: String = "Main"
+        static let login: String = "Login"
+        static let signUp: String = "SignUp"
+        static let homeStart: String = "HomeStart"
+    }
+    
+    struct ViewController {
+        static let login: String = "LoginViewController"
+        static let rainbow: String = "RainbowViewController"
+        static let signUp: String = "SignUpViewController"
+        static let homeStart: String = "HomeStartViewController"
+    }
+    
+    struct TableCells {
+        static let bookPage: String = "BookPageTableViewCell"
+    }
+}
