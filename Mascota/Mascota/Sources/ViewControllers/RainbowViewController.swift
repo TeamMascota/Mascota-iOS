855439ae06339e29db3200105d1496c9836a3c9c
Yoonseo Kim <yo26on@soongsil.ac.kr>
2021-07-04 13:05:25 +0900

    [#20] 샘플 RainbowViewController -> 다른 브랜치에서 작업 예정

--- Mascota/Mascota/Sources/ViewControllers/RainbowViewController.swift
+++ Mascota/Mascota/Sources/ViewControllers/RainbowViewController.swift
@@ -7,10 +7,30 @@
 
 import UIKit
 
-class RainbowViewController: UIViewController {
+import SnapKit
+import Then
 
+class RainbowViewController: BaseViewController {
+    
+    private lazy var tableView = UITableView().then {
+        $0.backgroundColor = .clear
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setView()
+        setNavigationBar(type: .home)
+        setBookTitleLabel(text: "헤헤헤헤헤헤헿헤헤헤ㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎ")
+    }
+    
+    private func setView() {
+        
+        view.addSubviews(tableView)
+        
+        tableView.snp.makeConstraints {
+            $0.top.equalTo(getTopBarBottomConstraint())
+            $0.trailing.leading.bottom.equalToSuperview()
+        }
         
     }
 
