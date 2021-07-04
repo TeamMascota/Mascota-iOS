868e8ce1b9ab27b7a64bf3879a8aefa12cc88ae2
Yoonseo Kim <yo26on@soongsil.ac.kr>
2021-07-04 18:02:54 +0900

    [#14] SceneDelegate에서 rootViewController 설정

--- Mascota/Mascota/Sources/SceneDelegate.swift
+++ Mascota/Mascota/Sources/SceneDelegate.swift
@@ -11,12 +11,25 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+       
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            self.window = window
+            
+            let navigationController = UINavigationController()
+            let viewcontroller = RainbowViewController()
+//             let viewcontroller = UIStoryboard(name: "StoryboardTest", bundle: Bundle(for: StoryboardViewController.self)).instantiateViewController(withIdentifier: "storyboardVC") as! StoryboardViewController
+            navigationController.viewControllers = [viewcontroller]
+            window.rootViewController = viewcontroller
+            window.backgroundColor = .macoIvory
+            window.makeKeyAndVisible()
+        }
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
