41623cc6b949eb3b3fc4009a7a0b230e26af8b0b
jeongyeob97 <leejeongyeob97@gmail.com>
2021-07-06 01:30:35 +0900

    [#42] 목차 상세 스토리보드와 뷰컨트롤러 생성

---
+++ Mascota/Mascota/Sources/Cells/IndexDetailTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  IndexDetailTableViewCell.swift
+//  Mascota
+//
+//  Created by apple on 2021/07/06.
+//
+
+import UIKit
+
+class IndexDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
