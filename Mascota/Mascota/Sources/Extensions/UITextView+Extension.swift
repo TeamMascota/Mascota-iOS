ed355ef199a8dfab4838af7d8884dc97dd9b8004
Yoonseo Kim <yo26on@soongsil.ac.kr>
2021-07-07 15:19:53 +0900

    [#46] setMacoTextView 수정

--- Mascota/Mascota/Sources/Extensions/UITextView+Extension.swift
+++ Mascota/Mascota/Sources/Extensions/UITextView+Extension.swift
@@ -8,11 +8,20 @@
 import UIKit
 
 extension UITextView {
-    func setConerRadius(color: UIColor? = .macoOrange) {
+    func setMacoTextView(color: UIColor? = .macoOrange) {
         layer.masksToBounds = true
         backgroundColor = .macoWhite
         
         layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
         layer.cornerRadius = Constant.round3
+        
+        tintColor = color
+    }
+    
+    func setUnderLine(color: UIColor? = .macoOrange) {
+        let border = CALayer()
+        border.frame = CGRect(x: 0, y: self.frame.size.height - 1, width: self.frame.width, height: 1)
+        border.backgroundColor = UIColor.macoBlue.cgColor
+        layer.addSublayer(border)
     }
 }
