a46191a740616a54a6bdb731b7cee4d8e7b821fb
Yoonseo Kim <yo26on@soongsil.ac.kr>
2021-07-07 16:03:58 +0900

    [#46] RainbowEpillogueViewController 레이아웃 구현 완료

--- Mascota/Mascota/Sources/ViewControllers/RainbowEpillogueViewController.swift
+++ Mascota/Mascota/Sources/ViewControllers/RainbowEpillogueViewController.swift
@@ -9,30 +9,21 @@ import UIKit
 
 class RainbowEpillogueViewController: UIViewController {
     
-    private lazy var rainbowNavigationBar = RainbowNavigationBarView(style: .leftAndRight, title: "작가의 말", subtitle: "에필로그")
+    private lazy var rainbowNavigationBar = RainbowNavigationBarView(style: .leftAndRight, title: "작가의 말", subtitle: "에필로그", underLineHidden: true)
     
-    private lazy var endButton = MacoButton(color: .white).then {
-        $0.setMacoButtonTitle("1부 마치기", for: .normal)
-        $0.setTitleColor(.macoGray, for: .disabled)
-        $0.setBackgroundColor(.macoLightGray, for: .disabled)
-    }
-    
-    private lazy var backgroundPageView = UIView().then {
+    private lazy var backgroundView = UIView().then {
         $0.backgroundColor = .macoIvory
     }
     
-    private lazy var  vLineView = UIView().then {
-        $0.backgroundColor = .macoBlue
+    private lazy var bookMarkImageView = UIImageView().then {
+        $0.image = .checkmark // 이미지 넣기
+        $0.backgroundColor = .blue
     }
     
-    private lazy var hLineView = UIView().then {
-        $0.backgroundColor = .macoBlue
-    }
-    
-    private lazy var titleLabel = UILabel().then {
-        $0.text = "1부를 마치며"
-        $0.font = .macoFont(type: .medium, size: 20)
-        $0.textColor = .macoDarkGray
+    private lazy var endButton = MacoButton(color: .blue).then {
+        $0.setMacoButtonTitle("이별의 단계 완료", for: .normal)
+        $0.setTitleColor(.macoGray, for: .disabled)
+        $0.setBackgroundColor(.macoLightGray, for: .disabled)
     }
     
     private lazy var contentLabel = UILabel().then {
@@ -42,37 +33,75 @@ class RainbowEpillogueViewController: UIViewController {
         $0.lineBreakMode = .byWordWrapping
     }
     
-    private lazy var writerSaidLabel = UILabel().then {
+    private lazy var epilogueTitleLabel = UILabel().then {
         $0.font = .macoFont(type: .medium, size: 17)
         $0.textColor = .macoDarkGray
-        $0.text = "작가의 말"
+        $0.text = "에필로그 제목"
+    }
+    
+    private lazy var epilogueTitleTextField = UITextField()
+    
+    private lazy var epilogueLabel = UILabel().then {
+        $0.font = .macoFont(type: .medium, size: 17)
+        $0.textColor = .macoDarkGray
+        $0.text = "에필로그"
     }
     
     private lazy var textView = UITextView().then {
-        $0.setConerRadius(color: .macoBlue)
-        $0.textContainerInset = UIEdgeInsets(top: 15, left: 12, bottom: 12, right: 15)
+        $0.setMacoTextView(color: .macoBlue)
+        $0.textContainerInset = UIEdgeInsets(top: 15, left: 12, bottom: 10, right: 12)
     }
     
-    private lazy var textViewUnderLineView = UIView().then {
-        $0.backgroundColor = .macoBlue
+    private lazy var continueLabel = UILabel().then {
+        let continueAttr = NSMutableAttributedString()
+        continueAttr.append("2부".convertColorFont(color: .macoDarkGray, fontSize: 14, type: .medium))
+        continueAttr.append("에서 계속".convertColorFont(color: .macoDarkGray, fontSize: 14, type: .regular))
+
+        $0.attributedText = continueAttr
+        
     }
+    
+    private lazy var name: String = "가나다라마바사"
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         initRainbowEpillogeuViewController()
         
+        setPetName()
+        
         setContentText()
-        setRainbowNavigationBar()
         setEndButton()
-        setBackgroundPageView()
         setLabels()
+        setTitleTextField()
         setTextView()
         
     }
     
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        textView.setUnderLine(color: .macoBlue)
+        epilogueTitleTextField.setMacoTextField(color: .macoBlue)
+    }
+    
     private func initRainbowEpillogeuViewController() {
         view.backgroundColor = .macoBlue
+        
+        setRainbowNavigationBar()
+       
+        view.addSubviews(backgroundView, bookMarkImageView)
+        
+        backgroundView.snp.makeConstraints {
+            $0.top.equalTo(rainbowNavigationBar.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        bookMarkImageView.snp.makeConstraints {
+            $0.top.equalTo(rainbowNavigationBar.snp.bottom)
+            $0.trailing.equalToSuperview().inset(15)
+            $0.width.equalTo(30)
+            $0.height.equalTo(85)
+        }
     }
     
     private func setRainbowNavigationBar() {
@@ -99,48 +128,35 @@ class RainbowEpillogueViewController: UIViewController {
        
     }
     
-    private func setBackgroundPageView() {
-        view.addSubviews(backgroundPageView, vLineView, hLineView)
-        
-        backgroundPageView.snp.makeConstraints {
-            $0.top.equalTo(rainbowNavigationBar.snp.bottom).offset(25)
-            $0.leading.trailing.equalToSuperview()
-            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(105)
-        }
-        
-        vLineView.snp.makeConstraints {
-            $0.top.equalTo(backgroundPageView.snp.top)
-            $0.bottom.equalTo(backgroundPageView.snp.bottom)
-            $0.leading.equalToSuperview().offset(16)
-            $0.width.equalTo(1)
-        }
-        
-        hLineView.snp.makeConstraints {
-            $0.leading.trailing.equalToSuperview()
-            $0.bottom.equalTo(backgroundPageView.snp.bottom).inset(10)
-            $0.height.equalTo(1)
-        }
-    }
-    
     private func setLabels() {
-        view.addSubviews(titleLabel, contentLabel, writerSaidLabel)
-        
-        titleLabel.snp.makeConstraints {
-            $0.leading.equalToSuperview().offset(36)
-            $0.top.equalTo(backgroundPageView.snp.top).offset(28)
-            $0.height.equalTo(20)
-        }
+        view.addSubviews(contentLabel, epilogueLabel)
         
         contentLabel.snp.makeConstraints {
-            $0.leading.equalTo(titleLabel.snp.leading)
-            $0.trailing.equalToSuperview().inset(20)
-            $0.top.equalTo(titleLabel.snp.bottom).offset(8)
+            $0.top.equalTo(rainbowNavigationBar.snp.bottom).offset(43)
+            $0.leading.equalToSuperview().offset(28)
+            $0.trailing.equalToSuperview().inset(28)
         }
         
-        writerSaidLabel.snp.makeConstraints {
-            $0.top.equalTo(contentLabel.snp.bottom).offset(30)
-            $0.leading.equalTo(titleLabel.snp.leading)
+    }
+    
+    private func setTitleTextField () {
+        epilogueTitleTextField.delegate = self
+        
+        epilogueTitleTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        
+        view.addSubviews(epilogueTitleLabel, epilogueTitleTextField)
+        
+        epilogueTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(contentLabel.snp.bottom).offset(51)
+            $0.leading.equalTo(contentLabel.snp.leading)
             $0.height.equalTo(17)
+        }
+
+        epilogueTitleTextField.snp.makeConstraints {
+            $0.top.equalTo(epilogueTitleLabel.snp.bottom).offset(8)
+            $0.leading.equalTo(contentLabel.snp.leading)
+            $0.trailing.equalTo(contentLabel.snp.trailing)
+            $0.height.equalTo(40)
         }
     }
     
@@ -151,29 +167,55 @@ class RainbowEpillogueViewController: UIViewController {
         
         setPlaceholder()
         
-        view.addSubviews(textView, textViewUnderLineView)
+        view.addSubviews(epilogueLabel, textView, continueLabel)
+        
+        epilogueLabel.snp.makeConstraints {
+            $0.top.equalTo(epilogueTitleTextField.snp.bottom).offset(28)
+            $0.leading.equalTo(contentLabel.snp.leading)
+            $0.height.equalTo(17)
+        }
 
         textView.snp.makeConstraints {
-            $0.top.equalTo(writerSaidLabel.snp.bottom).offset(9)
-            $0.bottom.equalTo(backgroundPageView.snp.bottom).inset(66)
-            $0.leading.equalTo(titleLabel.snp.leading)
-            $0.trailing.equalToSuperview().inset(16)
+            $0.top.equalTo(epilogueLabel.snp.bottom).offset(8)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(142)
+            $0.leading.equalTo(contentLabel.snp.leading)
+            $0.trailing.equalTo(contentLabel.snp.trailing)
         }
         
-        textViewUnderLineView.snp.makeConstraints {
-            $0.top.equalTo(textView.snp.bottom)
-            $0.leading.equalTo(textView.snp.leading)
-            $0.trailing.equalTo(textView.snp.trailing)
-            $0.height.equalTo(1)
+        continueLabel.snp.makeConstraints {
+            $0.top.equalTo(textView.snp.bottom).offset(8)
+            $0.trailing.equalTo(contentLabel.snp.trailing)
+        }
+    }
+    
+    private func setContentText() {
+        contentLabel.text = "이별을 마무리하는 마지막 단계예요.\n\(name)의 행복한 시간들을 잘 보셨나요?\n작가님과 함께했던 코봉코봉코봉이의 삶이 어땠는지\n적어주세요. 그리고 이제는 작가님의 이야기를\n들려주세요."
+        contentLabel.setLineSpacing(lineHeight: 20)
+    }
+    
+    private func setPetName() {
+        name = "뮨서"
+    }
+    
+    @objc
+    func textFieldDidChange(_ textField: UITextField) {
+        endButton.isEnabled = checkEndButtonEnabled()
+    }
+    
+    func checkEndButtonEnabled() -> Bool {
+        guard let text = epilogueTitleTextField.text else { return false }
+        
+        if text.isEmpty || textView.text == "\(name)의 이야기를 마무리하는 글을 적어주세요." {
+            return false
+        } else {
+            return true
         }
         
     }
     
-    private func setContentText() {
-        contentLabel.text = "이별을 마무리하는 마지막 단계예요.\n코봉의 행복한 시간들을 잘 보셨나요?\n작가님과 함께했던 코봉코봉코봉이의 삶이 어땠는지\n적어주세요. 그리고 이제는 작가님의 이야기를\n들려주세요."
-        
-        contentLabel.setLineSpacing(lineSpacing: 5)
-    }
+}
+
+extension RainbowEpillogueViewController: UITextFieldDelegate {
     
 }
 
@@ -186,30 +228,26 @@ extension RainbowEpillogueViewController: UITextViewDelegate {
     }
     
     func textViewDidEndEditing(_ textView: UITextView) {
-
         if textView.text.isEmpty {
             setPlaceholder()
         }
     }
-
+    
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
         return true
     }
     
     func textViewDidChange(_ textView: UITextView) {
         textView.attributedText = textView.text.attributedString(font: .macoFont(type: .regular, size: 16), color: .macoBlack, customLineHeight: 28)
-        if !textView.text.isEmpty {
-            endButton.isEnabled = true
-        } else {
-            endButton.isEnabled = false
-        }
+        endButton.isEnabled = checkEndButtonEnabled()
+        
     }
     
     func setPlaceholder() {
-        textView.text = "코봉이d의 이야기를 들려주세요."
+        textView.text = "\(name)의 이야기를 마무리하는 글을 적어주세요."
         textView.textColor = .macoGray
         textView.font = .macoFont(type: .regular, size: 14)
         
-        endButton.isEnabled = false
+        endButton.isEnabled = checkEndButtonEnabled()
     }
 }
