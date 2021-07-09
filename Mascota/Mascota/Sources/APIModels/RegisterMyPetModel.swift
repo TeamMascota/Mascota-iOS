80a5dc24ef922283c1543476820224a8fb6d5c39
YeonSeoDo <ehdustj1001@naver.com>
2021-07-09 13:48:48 +0900

    [#29] RegisterMyPet API모델 추가

--- Mascota/Mascota/Sources/APIModels/RegisterMyPetModel.swift
+++ Mascota/Mascota/Sources/APIModels/RegisterMyPetModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+// MARK: - RegisterMyPetModel
+struct RegisterMyPetModel: Codable {
+    let pets: [Pet]
+    let userID: String
+
+    enum CodingKeys: String, CodingKey {
+        case pets
+        case userID = "userId"
+    }
+}
+
+// MARK: - Pet
+struct Pet: Codable {
+    let petImages: String
+    let name: String
+    let kind: Int
+    let startDate: String
+    let gender: Int
+}
