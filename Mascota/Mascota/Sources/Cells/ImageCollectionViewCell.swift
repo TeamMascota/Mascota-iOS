19fce96c58c0fafb34f23e7e309ca0920206fc4c
jeongyeob97 <leejeongyeob97@gmail.com>
2021-07-13 03:04:26 +0900

    [#65] DiaryWriteSecond Storyboard 구성

---
+++ Mascota/Mascota/Sources/Cells/ImageCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCollectionViewCell.swift
+//  Mascota
+//
+//  Created by apple on 2021/07/13.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Mascota/Mascota/Sources/Cells/PlusCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PlusCollectionViewCell.swift
+//  Mascota
+//
+//  Created by apple on 2021/07/13.
+//
+
+import UIKit
+
+class PlusCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
