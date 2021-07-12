5017787b74973746bb7a3c72da90c0f0f3b73fb2
jeongyeob97 <leejeongyeob97@gmail.com>
2021-07-12 23:55:00 +0900

    [#65] DiaryWriteStoryboard 구성

---
+++ Mascota/Mascota/Sources/ViewControllers/DIaryWriteSecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DIaryWriteSecondViewController.swift
+//  Mascota
+//
+//  Created by apple on 2021/07/12.
+//
+
+import UIKit
+
+class DIaryWriteSecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
