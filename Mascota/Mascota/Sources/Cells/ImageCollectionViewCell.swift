f22f6c3f2ad262a4754c83e7b591dd9d55a1fb59
jeongyeob97 <leejeongyeob97@gmail.com>
2021-07-13 04:25:15 +0900

    [#65] DiaryWriteSecond 텍스트뷰 구현

--- Mascota/Mascota/Sources/Cells/ImageCollectionViewCell.swift
+++ Mascota/Mascota/Sources/Cells/ImageCollectionViewCell.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class ImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var symbolImageView: UIImageView!
+    @IBOutlet weak var userImageView: UIImageView!
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        self.layer.cornerRadius = 3
     }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+    func initializeCell(data: String?){
+        if let string = data {
+//            userImageView.image = dasda
+            self.layer.borderWidth = 1
+            self.layer.borderColor = UIColor.macoOrange.cgColor
+            self.symbolImageView.isHidden = true
+        } else {
+            self.symbolImageView.isHidden = false
+        }
+    }
+    
 
 }
--- Mascota/Mascota/Sources/ViewControllers/DIaryWriteSecondViewController.swift
+++ Mascota/Mascota/Sources/ViewControllers/DIaryWriteSecondViewController.swift
@@ -8,27 +8,79 @@
 import UIKit
 
 class DiaryWriteSecondViewController: UIViewController {
-    @IBOutlet weak var scrollViewBackgroundView: UIView!
     
+    let textViewPlaceholder: NSAttributedString = "오늘 주인공에게 어떤 일이 있었나요?".convertColorFont(color: UIColor.macoLightGray, fontSize: 14, type: .regular)
+    
+    var writtenJournal: String = ""
+    
+    @IBOutlet weak var scrollViewBackgroundView: UIView!
+    @IBOutlet weak var titleView: UIView!
+    @IBOutlet weak var dateView: UIView!
+    @IBOutlet weak var indexView: UIView!
+    @IBOutlet weak var indexLabel: UILabel!
+    @IBOutlet weak var indexTitleLabel: UILabel!
     @IBOutlet weak var imageCollectionView: UICollectionView!
     @IBOutlet weak var indexToggleButton: UIButton!
     @IBOutlet weak var dateTextField: UITextField!
-    @IBOutlet weak var titleLabel: UITextField!
+    @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var titleCountLabel: UILabel!
     @IBOutlet weak var journalTextView: UITextView!
     
     
+    // 스크롤뷰에서 먹히지 않기 때문에 scrollView에 탭 제스처를 붙여서 처리해줘야 됨
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        super.touchesBegan(touches, with: event)
+//        print(1)
+//        self.view.endEditing(true)
+//    }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-//        registerCollectionView()
-//        registerCollectionViewCell()
+        setComponents()
+        registerCollectionView()
+        registerCollectionViewCell()
+        verifyTextView()
+        registerTextView()
         
+    }
+    
+    private func registerTextView() {
+        journalTextView.delegate = self
+    }
+    
+    private func verifyTextView() {
+        if self.journalTextView.text?.count == 0 {
+            self.journalTextView.attributedText = textViewPlaceholder
+        }
+    }
+    private func setComponents() {
+        self.indexLabel.isHidden = true
+        self.indexTitleLabel.isHidden = true
+        self.dateTextField.placeholder = findToday()
+        self.dateView.round(corners: [.topLeft, .topRight], cornerRadius: 3)
+        self.titleView.round(corners: [.topLeft, .topRight], cornerRadius: 3)
+        self.journalTextView.round(corners: [.topLeft, .topRight], cornerRadius: 3)
+        
+    }
+    
+    private func findToday() -> String {
+        let today = Date()
+        
+        let date = DateFormatter()
+        date.locale = Locale(identifier: "ko_kr")
+        date.dateFormat = "yyyy.MM.dd"
+
+        return date.string(from: today)
     }
     
     private func registerCollectionViewCell() {
         self.imageCollectionView.register(UINib(nibName: AppConstants.CollectionViewCells.plusCollectionViewCell,
                                                 bundle: nil),
                                           forCellWithReuseIdentifier: AppConstants.CollectionViewCells.plusCollectionViewCell)
+        
+        self.imageCollectionView.register(UINib(nibName: AppConstants.CollectionViewCells.imageCollectionViewCell,
+                                                bundle: nil),
+                                          forCellWithReuseIdentifier: AppConstants.CollectionViewCells.imageCollectionViewCell)
     }
     
     private func registerCollectionView() {
@@ -65,14 +117,44 @@ extension DiaryWriteSecondViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AppConstants.CollectionViewCells.plusCollectionViewCell,
-                                                            for: indexPath) as? PlusCollectionViewCell else {
+        
+        if indexPath.item == 0 {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AppConstants.CollectionViewCells.plusCollectionViewCell,
+                                                                for: indexPath) as? PlusCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+            return cell
+        }
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AppConstants.CollectionViewCells.imageCollectionViewCell, for: indexPath) as? ImageCollectionViewCell else {
             return UICollectionViewCell()
         }
-        cell.layer.borderWidth = 1
-        cell.layer.borderColor = UIColor.macoBlack.cgColor
+        cell.initializeCell(data: nil)
         return cell
     }
     
     
+}
+
+extension DiaryWriteSecondViewController: UITextViewDelegate {
+    
+    func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
+    
+        if textView.attributedText == textViewPlaceholder {
+            textView.text = ""
+            textView.font = UIFont.macoFont(type: .regular, size: 16)
+            textView.textColor = .macoBlack
+        }
+        return true
+    }
+    
+    func textViewShouldEndEditing(_ textView: UITextView) -> Bool {
+        if textView.text?.count == 0 {
+            textView.attributedText = textViewPlaceholder
+        }
+        
+        
+        
+        return true
+    }
 }
