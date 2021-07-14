b9e80137cd1e93f09ed579b98a8f9db54dca5073
Yoonseo Kim <yo26on@soongsil.ac.kr>
2021-07-15 03:12:12 +0900

    [#90] MemoryModel 정의

---
+++ Mascota/Mascota/Sources/APIModels/MemoryModel.swift
@@ -0,0 +1,11 @@
+//
+//  MemoryModel.swift
+//  Mascota
+//
+//  Created by 김윤서 on 2021/07/15.
+//
+
+struct MemoryModel: Codable {
+    let title, contents, date, diaryId: String
+    let feeling: Int
+}
