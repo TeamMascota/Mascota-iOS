cae4c354c8785f7f3496f1a70e683bfa32d7156d
Yoonseo Kim <yo26on@soongsil.ac.kr>
2021-07-15 22:55:57 +0900

    [#94] PutPartingRainbowBridgeModel

---
+++ Mascota/Mascota/Sources/APIModels/PutPartingRainbowBridge.swift
@@ -0,0 +1,10 @@
+//
+//  PutPartingRainbowBridge.swift
+//  Mascota
+//
+//  Created by 김윤서 on 2021/07/15.
+//
+
+struct PutPartingRainbowBridgeModel: Codable {
+    let partingRainbowBridge: PartingRainbowBridgeModel
+}
