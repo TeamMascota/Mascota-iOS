bf2ae0c14661e755b440a9760c1dd6788d149482
Yoonseo Kim <yo26on@soongsil.ac.kr>
2021-07-15 22:54:36 +0900

    [#94] DiaryModel

---
+++ Mascota/Mascota/Sources/APIModels/DiaryModel.swift
@@ -0,0 +1,11 @@
+//
+//  DiaryModel.swift
+//  Mascota
+//
+//  Created by 김윤서 on 2021/07/15.
+//
+
+struct DiaryModel: Codable {
+    let chapter, episode: Int?
+    let title, contents, date: String?
+}
