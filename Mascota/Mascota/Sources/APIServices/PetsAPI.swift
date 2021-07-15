ea0cbf084bb5ab233729de292158f0f66617aacd
jeongyeob97 <leejeongyeob97@gmail.com>
2021-07-16 01:24:57 +0900

    [#100] Pet API와 데이터 저장을 위한 구조체 구현

---
+++ Mascota/Mascota/Sources/APIServices/PetsAPI.swift
@@ -0,0 +1,58 @@
+//
+//  PetsAPI.swift
+//  Mascota
+//
+//  Created by apple on 2021/07/16.
+//
+
+import Foundation
+import Moya
+
+enum PetsAPI {
+    case getPetsInfo
+}
+
+extension PetsAPI: TargetType {
+    var baseURL: URL {
+        guard let url = URL(string: APIService.baseURL + "/pet") else {
+            fatalError()
+        }
+        return url
+    }
+    
+    var path: String {
+        switch self {
+        case .getPetsInfo:
+            return "/register/petInfo"
+        default:
+            return ""
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getPetsInfo:
+            return .get
+        default:
+            return .get
+        }
+    }
+    
+    var sampleData: Data {
+        return Data()
+    }
+    
+    var task: Task {
+        switch self {
+        case .getPetsInfo:
+            return .requestPlain
+        default:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        return nil
+    }
+    
+}
---
+++ Mascota/Mascota/Sources/AppModels/DiaryWriteModel.swift
@@ -0,0 +1,16 @@
+//
+//  DiaryWriteModel.swift
+//  Mascota
+//
+//  Created by apple on 2021/07/16.
+//
+
+import UIKit
+
+struct DiaryWriteModel {
+    let title: String
+    let diaryImages: [UIImage]
+    let contents: String
+    let date: String
+    let id: String // 목차 아이디
+}
