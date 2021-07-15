5d87cc6126883734ac82cc877e2a01dbedd72ec5
Yoonseo Kim <yo26on@soongsil.ac.kr>
2021-07-15 22:55:15 +0900

    [#94] GetRainbowRecordModel

---
+++ Mascota/Mascota/Sources/APIModels/GetRainbowRecordModel.swift
@@ -0,0 +1,11 @@
+//
+//  GetRainbowRecordModel.swift
+//  Mascota
+//
+//  Created by 김윤서 on 2021/07/15.
+//
+
+struct GetRainbowRecordModel: Codable {
+    let diaryCount, dayTogether: Int
+    let bookInfo: BookInfoMoel
+}
