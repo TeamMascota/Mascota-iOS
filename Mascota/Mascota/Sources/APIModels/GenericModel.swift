35f1460141b4b4df93e451a615e578b08409bb41
yoonseo <yo26on@soongsil.ac.kr>
2021-07-16 18:08:00 +0900

    [#98] GenericModel 수정

--- Mascota/Mascota/Sources/APIModels/GenericModel.swift
+++ Mascota/Mascota/Sources/APIModels/GenericModel.swift
@@ -13,7 +13,7 @@ struct GenericModel<T: Codable>: Codable {
     
     init(from decoder: Decoder) throws {
         let value = try decoder.container(keyedBy: CodingKeys.self)
-        status = (try? value.decode(Int.self, forKey: .success)) ?? 0
+        status = (try? value.decode(Int.self, forKey: .status)) ?? 0
         success = (try? value.decode(Bool.self, forKey: .success)) ?? false
         message = (try? value.decode(String.self, forKey: .message)) ?? ""
         data = (try? value.decode(T.self, forKey: .data))
