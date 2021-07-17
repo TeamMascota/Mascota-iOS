c4cdc28d1388a72c8cc8a0d803e44ce5701195d9
jeongyeob97 <leejeongyeob97@gmail.com>
2021-07-17 13:14:17 +0900

    [#133] 일기 작성 완료

--- Mascota/Mascota/Sources/AppModels/CharacterModel.swift
+++ Mascota/Mascota/Sources/AppModels/CharacterModel.swift
@@ -10,6 +10,11 @@ import Foundation
 struct CharacterModel: Codable {
     let id: String
     let feeling: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case feeling
+    }
 }
 
 
